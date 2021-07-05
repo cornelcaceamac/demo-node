57898180e9bb199b1f42ee1dce6ba2aeb707f707
Cornel Caceamacm <caceamac.cornel@yahoo.com>
2021-07-05 14:54:41 +0300

    small update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hi there');
+    res.send('Hi there from second one');
 });
 
 app.listen(5000, ()=>{
